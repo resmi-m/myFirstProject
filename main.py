d17eb07447c8df2e4945814dc8130da04cd76b80
RESMI <resmimuca1@gmail.com>
2023-04-02 20:28:52 +0300

    TOP SELLING LIQUOR PER ZIP CODE

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+import pandas as pd
+import numpy as np
+df = pd.read_csv('liquer_sales_2016-2019.csv')
+df = pd.DataFrame(df)
+groupped_by_zip = df.groupby('zip_code')
+print(groupped_by_zip['county','item_description','bottles_sold','sale_dollars'].aggregate(np.max))
+
+
+
+
