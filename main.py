6aefc78779e413fca830a20d472cdf1cf7dbc104
RESMI <resmimuca1@gmail.com>
2023-04-03 00:51:44 +0300

    final

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ total_sales = df['sale_dollars'].sum()
 df1 = df.groupby('store_name').sum().reset_index()
 df1['%_total'] = (df1['sale_dollars']/total_sales)*100
 print(df1)
-df2 = df.groupby('zip_code')
-print(df2.aggregate('sale_dollars':'sum'))
+df2 = df.groupby('zip_code').sum()
+print(df2)
 df1.to_csv('df1', encoding='utf-8', index=False)
 df2.to_csv('df2', encoding='utf-8', index=False)
 
