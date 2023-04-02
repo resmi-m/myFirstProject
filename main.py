991a82ac6b659feee26690c32cc1d1c1171b316b
RESMI <resmimuca1@gmail.com>
2023-04-03 00:38:57 +0300

    final

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import numpy as np
+import csv
 df = pd.read_csv('liquer_sales_2016-2019.csv')
 df = pd.DataFrame(df)
 total_sales = df['sale_dollars'].sum()
-df1 = df.groupby('store_name')['sale_dollars'].sum().reset_index()
+df1 = df.groupby('store_name').sum().reset_index()
 df1['%_total'] = (df1['sale_dollars']/total_sales)*100
 print(df1)
-df2 = df.groupby('zip_code')['sale_dollars'].max()
-print(df2)
+df2 = df.groupby('zip_code')
+print(df2.aggregate('sale_dollars':'sum'))
+df1.to_csv('df1', encoding='utf-8', index=False)
+df2.to_csv('df2', encoding='utf-8', index=False)
+
+
 
 
 
